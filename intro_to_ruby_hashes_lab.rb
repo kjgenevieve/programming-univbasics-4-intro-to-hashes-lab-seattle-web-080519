2a2b4cee9e396bf219e500f5662f7566e20fafe7
Genevieve McAllister <kj.genevieve@gmail.com>
2019-07-18 17:30:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,6 @@ def my_hash_creator(key, value)
   hash = Hash.new
   hash[key] = value
   hash
-  # return a hash that includes the key and value parameters passed into this method
-  
 end
 
 def read_from_hash(hash, key)
